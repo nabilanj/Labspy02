7a5a4794afe49109d63d22d904229fd533e3551e
Nabila Nurmiftahul Janah <57028453+nabilanj@users.noreply.github.com>
2019-10-30 00:01:28 +0700

    Python File

---
+++ Project.if.py
@@ -0,0 +1,14 @@
+K = int (input (" Masukkan Tensi Mahasiswa ke 1 : "))
+L = int (input (" Masukkan Tensi Mahasiswa ke 2 : "))
+M = int (input (" Masukkan Tensi Mahasiswa ke 3 : "))
+
+if K > L and K > M :
+    print ("Mahasiswa Tensi Tertinggi : ", K)
+if L > K and L > M :
+    print ("Mahasiswa Tensi Tertinggi : ", L)
+if M > K and M > L :
+    print ("Mahasiswa Tensi Tertinggi : ", M)
+    
+    
+    
+    
